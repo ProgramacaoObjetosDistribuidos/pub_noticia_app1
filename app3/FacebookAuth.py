1d4ccf0437e682ea4fdfdd46fcd989b3059ba372
João Marcos F <joaomarccos.ads@gmail.com>
2015-06-11 23:13:04 -0300

    conversor de XML para respostado app2 para app3

--- app3/FacebookAuth.py
+++ app3/FacebookAuth.py
@@ -8,15 +8,15 @@ import sys
 
 FB_TOKEN_FILE = 'access_token.txt'
 
-class Browser(object):
 
+class Browser(object):
     def __init__(self, app_key, token_file=FB_TOKEN_FILE):
         self.debug = False
         self.close_window = True
         self.token_file = token_file
         self.token = ''
 
-        #app
+        # app
         self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
         self.scrolled_window = gtk.ScrolledWindow()
         # Cria a webview
@@ -25,30 +25,29 @@ class Browser(object):
         self.window.add(self.scrolled_window)
         # Coonectando eventos
         self.window.connect('destroy', self._destroy_event_cb)
-        self.web_view.connect('load-committed', self._load_committed_cb) # evento de redirecionamento carregado
+        self.web_view.connect('load-committed', self._load_committed_cb)  # evento de redirecionamento carregado
         self.window.set_default_size(1024, 800)
         # carrega pagina Facebook usando OAuth
         self.web_view.load_uri(
-            'https://www.facebook.com/dialog/oauth?client_id=%s&redirect_uri=%s&response_type=token&scope=email' % (urllib.quote(app_key), urllib.quote('https://www.facebook.com/connect/login_success.html'))
-            )
+            'https://www.facebook.com/dialog/oauth?client_id=%s&redirect_uri=%s&response_type=token&scope=email' % (
+                urllib.quote(app_key), urllib.quote('https://www.facebook.com/connect/login_success.html'))
+        )
 
     def _load_committed_cb(self, web_view, frame):
 
-		# Pega a url corrente da webview e checa se foi redirecionado para https://www.facebook.com/connect/login_success.html
-		# parametros como "access_token" são recuperados via get :)
+
         uri = frame.get_uri()
         parse = urlparse.urlparse(uri)
         if (hasattr(parse, 'netloc') and hasattr(parse, 'path') and
-            hasattr(parse, 'fragment') and parse.netloc == 'www.facebook.com' and
-            parse.path == '/connect/login_success.html' and parse.fragment):
+                hasattr(parse, 'fragment') and parse.netloc == 'www.facebook.com' and
+                    parse.path == '/connect/login_success.html' and parse.fragment):
             # pega token da URL
             params = urlparse.parse_qs(parse.fragment)
-            self.token = params['access_token'][0] # aqui pego o parametro access_token que está na Url :) só alegria
-            # faço o que quiser com ele > salvei no arquivo.
+            self.token = params['access_token'][0]
             token_file = open(self.token_file, 'w')
             token_file.write(self.token)
             token_file.close()
-            gtk.main_quit() # Finish
+            gtk.main_quit()  # Finish
             if self.close_window:
                 try:
                     self.window.destroy()
@@ -61,6 +60,7 @@ class Browser(object):
     def authorize(self):
         self.window.show_all()
         gtk.main()
+
 
 if (__name__ == '__main__'):
     # Creates the browser
---
+++ app3/Loader.py
@@ -0,0 +1,24 @@
+__author__ = 'joaomarcos'
+import xml.etree.ElementTree as ET
+
+class Response:
+
+    def fromXml(self, xml):
+        root = ET.fromstring(xml)
+        for child in root:
+            if child.tag == 'session':
+                self.session = child.text
+            elif child.tag == 'name':
+                self.name = child.text
+            elif child.tag == 'email':
+                self.email = child.text
+            elif child.tag == 'error':
+                self.error = child.text
+
+if (__name__ == '__main__'):
+    with open('response.xml', 'rb') as xml_file:
+        xml = xml_file.read()
+        response = Response()
+        response.fromXml(xml)
+
+        print response.name
